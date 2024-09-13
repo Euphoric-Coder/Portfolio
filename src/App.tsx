55105d04d4c56a7045a4e087371638e5164ca9f9
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-09-13 06:47:18 +0530

    Pre-Deploy Stage Reached

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 function App() {
   useEffect(()=>{
